adc43e1cd06e760303ec509b2e7702fda83dd59e
Ashkan <as6kan.developer@gmail.com>
2025-04-24 20:29:09 +0330

    fix: users directories

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { UsersModule } from './users/users.module';
+import { UsersModule } from './admin/users/users.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 
